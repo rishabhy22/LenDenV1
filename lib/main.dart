4461b0305abbdf9848ec2347f4813a8bdda49535
rishabhy22 <rtgjc22@gmail.com>
2019-10-30 22:12:57 +0530

    first modified commit

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class RandomWordsState extends State<RandomWords> {
               onPressed: () {
                 setState(() {});
               }),
-          alignment: Alignment.bottomCenter,
+          alignment: Alignment.bottomRight,
         )
       ],
     );
